67ed9927e871cd290b384bfbe58061d646d34c4e
a.s.cardoso1998@gmail.com <Abilio.Fonseca@ctw.bmwgroup.com>
2025-09-03 17:09:11 +0100

    starting modules

---
+++ Modules/main.py
@@ -0,0 +1,4 @@
+#with an import like this we need to put the module1 prefix because we imported the functions inside, inside the namespace module1 not to this "file" namespace
+import module_1
+module_1.print_name("abilio")
+module_1.print_name_in_portuguese("abilio")
