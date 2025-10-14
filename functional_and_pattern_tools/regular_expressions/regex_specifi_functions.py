6149e981e2251484e6bd367cc90ef71894d134d6
a.s.cardoso1998@gmail.com <Abilio.Fonseca@ctw.bmwgroup.com>
2025-10-14 10:08:50 +0100

    finished regular_expressions

--- functional_and_pattern_tools/regular_expressions/regex_specifi_functions.py
+++ functional_and_pattern_tools/regular_expressions/regex_specifi_functions.py
@@ -1,7 +1,18 @@
-import re
+import re  # Import the regular expressions module
 
-# groups
+# Sample string containing an email-like pattern
 test_string = 'abilio-fonseca@github.com'
-match = re.search(r'[\w.-]@[\w.-]+',test_string)
 
-print(match.group())
+# Use re.search to find the first substring in test_string that matches the pattern:
+# Pattern breakdown:
+# [\w.-]+    => Match one or more (+) characters that are either:
+#               - word characters (letters, digits, underscore),
+#               - dot ('.'),
+#               - or dash ('-')
+# @          => Match the '@' symbol literally
+# [\w.-]+    => Match one or more (+) characters as above (the domain part)
+match = re.search(r'[\w.-]+@[\w.-]+', test_string)
+
+# If a match is found, print the matched substring
+if match:
+    print(match.group())
